b4c785172cf370149df84d94d57712a9f78f8151
siddeshsarathy <47939983+siddeshsarathy@users.noreply.github.com>
2025-03-01 00:25:49 +1100

    Update firebase-admin.config.ts

--- src/utils/config/firebase-admin.config.ts
+++ src/utils/config/firebase-admin.config.ts
@@ -1,13 +1,11 @@
 import admin from "firebase-admin";
 
-if (process.env.FIREBASE_DISABLED === "true") {
-  console.log("🚨 Firebase Admin is disabled for this deployment.");
-} else {
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-    });
-  }
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
 }
 
-export default admin;
+const firestore = admin.firestore();
+
+export { firestore };
